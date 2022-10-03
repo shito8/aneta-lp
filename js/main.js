51669d1c1bba9068c13da5a9801fdd63b92c1d8c
esis8 <eesis.dev@gmail.com>
2022-10-03 18:18:06 -0300

    apply carousel

--- js/main.js
+++ js/main.js
@@ -34,3 +34,17 @@ active.addEventListener("mouseout", ()=>{
     active.classList.add('mActive');
     });
 
+// ---------- 
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const elementCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementCarousel, {
+        duration: 150,
+        dist: -40,
+        shift: 50,
+        padding: 50,
+        numVisible: 3,
+        indicators: true,
+        noWrap: false
+    });
+});
